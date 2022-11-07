8053c64f12efb415e134f22fc61be5ca30cbdc30
Kaelyn Hughes <kaelynmhughes@gmail.com>
2022-11-06 19:44:23 -0700

    Finished research tab

--- app/src/main/java/com/kaelynhughes/assignment4/ResearchFragment.kt
+++ app/src/main/java/com/kaelynhughes/assignment4/ResearchFragment.kt
@@ -24,9 +24,56 @@ class ResearchFragment : Fragment() {
     ): View? {
         val binding = FragmentResearchBinding.inflate(inflater, container, false)
 
-        val research = (0 until 100).map {
-            ResearchArea("Area ${it}", "doing ${it}", "Person ${it}")
-        }.toList()
+        val research = listOf(
+            ResearchArea("Artificial Intelligence",
+                "Multi-agent systems, physics informed deep learning, criticality in natural systems, " +
+                        "automatic differentiation, deep neural networks, AI for social impact, robotics, " +
+                        "decision science, precision apiculture, precision agriculture, assistive technology, " +
+                        "PIV/PTV, and computability.",
+                "Faculty: Vicki Allan, Nick Flann, Hamid Karimi, Mario Harper, and Vladimir Kulyukin"),
+            ResearchArea("Algorithms",
+                "Planning algorithms, optimization algorithms, simulation, and computational geometry.",
+                "Faculty: John Edwards, Mario Harper, and Haitao Wang"),
+            ResearchArea("Augmented Virtual Reality",
+                "3D user interfaces, human perception and cognition in VR and AR.",
+                "Faculty: Isaac Cho"),
+            ResearchArea("Computer Science Education",
+                "Educational data mining, keystroke analysis, and educational psychology.",
+                "Faculty: John Edwards"),
+            ResearchArea("Computer Vision", "Computer vision, pattern recognition, " +
+                    "breast ultrasound (BUS) image processing, pavement crack detection/classification, " +
+                    "uncertainty theories & new logics,  meta-learning, deep learning for medical Information processing, " +
+                    "medical image segmentation, deep learning-based object detection & recognition, " +
+                    "morphed face detection,  graph-based deep learning, and machine learning.",
+                "Faculty: Hengda Cheng and Xiaojun Qi"),
+            ResearchArea("Cybersecurity",
+                "Human aspects of security, and privacy perceptions of users.",
+                "Faculty: Mahdi Nasrullah Al-Ameen"),
+            ResearchArea("Data Science and Data Mining",
+                "Applied data mining for space weather research and pattern discovery from " +
+                        "large time series data, social network analysis, graph mining, social media mining, " +
+                        "educational data mining, machine learning, deep learning, representation learning " +
+                        "of graphs and multivariate time series, ML cyberinfrastructure development, " +
+                        "deep learning and its applications in anomaly detection, and natural language processing.",
+                "Faculty: Soukaina Filali Boubrahimi, Hamid Karimi, Shah Muhammad Hamdi, and Shuhan Yuan"),
+            ResearchArea("Data Visualization and Visual Analytics",
+                "Decision-making and sense making, user behavior analytics, text analytics, and data analytics.",
+                "Faculty: Isaac Cho"),
+            ResearchArea("Database Systems",
+                "Temporal databases, hierarchical databases, and biodiversity databases.",
+                "Faculty: Curtis Dyreson"),
+            ResearchArea("High Performance Computing and Scientific Visualization",
+                "Big data analysis and visualization, distributed and parallel computing, " +
+                        "topological analysis, cyberinfrastructure, cloud computing, and simulation visualization.",
+                "Faculty:  John Edwards, Steve Petruzza"),
+            ResearchArea("Human-Computer Interaction",
+                "User behavior and challenges in technology use and corresponding workarounds, " +
+                        "virtual reality, augmented reality, and 3D user interfaces.",
+                "Faculty: Mahdi Nasrullah Al-Ameen, Isaac Cho"),
+            ResearchArea("Information and Communication Technology for Development",
+                "Usability issues and privacy concerns around digital technology use in developing countries.",
+                "Faculty: Mahdi Nasrullah Al-Ameen")
+        )
 
         binding.researchRecyclerView.adapter = ResearchAdapter(research)
         binding.researchRecyclerView.layoutManager = LinearLayoutManager(this.context)
